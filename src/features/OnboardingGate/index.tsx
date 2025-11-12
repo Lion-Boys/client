8faedab7a2a95994483af80408b770ca5502bc82
Dohyeop Lim <dhlim0817@gmail.com>
2025-11-12 17:51:45 +0900

    feat: impl. onboarding gate

---
+++ src/features/OnboardingGate/index.tsx
@@ -0,0 +1,50 @@
+import { useEffect } from "react";
+import { useUserFlowStore } from "@/store/useUserFlowStore";
+import { useUiStore } from "@/store/useUIStateStore";
+import BottomButton from "@/components/BottomButton";
+
+export default function OnboardingGate({ children }: React.PropsWithChildren) {
+    const hasOnboarded = useUserFlowStore((s) => s.hasOnboarded);
+    const completeOnboarding = useUserFlowStore((s) => s.completeOnboarding);
+
+    const setShowGradientBackground = useUiStore((s) => s.setShowGradientBackground);
+
+    useEffect(() => {
+        if (!hasOnboarded) {
+            setShowGradientBackground(true);
+            return () => setShowGradientBackground(false);
+        } else {
+            setShowGradientBackground(false);
+        }
+    }, [hasOnboarded, setShowGradientBackground]);
+
+    const handleStart = () => {
+        completeOnboarding();
+        setShowGradientBackground(false);
+    };
+
+    if (!hasOnboarded) {
+        return (
+            <div className="h-full flex flex-col items-center justify-end gap-28">
+                <div className="grid place-items-center gap-4">
+                    <h2 className="font-text-2xl-24_semibold text-white text-center">
+                        모임 준비, 진행, 마무리의
+                        <br />
+                        모든 순간에 사용하세요
+                    </h2>
+
+                    <p className="font-text-base-16_regular text-grey-100 text-center">
+                        회차 참석 인원 체크부터 사진 공유,
+                        <br />
+                        송금 확인까지 빈틈없이 관리하는
+                        <br />
+                        올인원 회식 정산 시스템
+                    </p>
+                </div>
+                <BottomButton onClick={handleStart} label="시작하기" />
+            </div>
+        );
+    }
+
+    return <>{children}</>;
+}
