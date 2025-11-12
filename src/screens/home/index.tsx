f77b26e58fff6ff0da7b4815c436b5e752e3df2c
Dohyeop Lim <dhlim0817@gmail.com>
2025-11-12 22:57:25 +0900

    feat: add demo entry

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 import { useUiStore } from "@/store";
+import { Link } from "react-router-dom";
+import BottomButton from "@/components/BottomButton";
 
 export default function Home() {
     const updateHeader = useUiStore((s) => s.updateHeader);
@@ -9,8 +11,10 @@ export default function Home() {
     }, [updateHeader]);
 
     return (
-        <>
-            <h1>Home</h1>
-        </>
+        <div className="h-full grid place-items-center">
+            <Link to="/party/new">
+                <BottomButton label="모임 만들기" />
+            </Link>
+        </div>
     );
 }
