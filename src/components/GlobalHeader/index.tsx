d57572380532d544c8bbf483590be11197353e4d
Dohyeop Lim <dhlim0817@gmail.com>
2025-11-13 14:47:31 +0900

    feat: change root bg

--- src/components/GlobalHeader/index.tsx
+++ src/components/GlobalHeader/index.tsx
@@ -8,7 +8,9 @@ export default function GlobalHeader() {
     return (
         <div
             className={`fixed w-full h-13.5 grid place-items-center z-9999 ${
-                showGradientBackground ? "bg-transparent text-white" : "bg-white text-grey-900_text"
+                showGradientBackground
+                    ? "bg-transparent text-white"
+                    : "bg-grey-50 text-grey-900_text"
             }`}
         >
             <p className="font-text-lg-18_semibold select-none">{title}</p>
