de8c506e27a2dd25a37b69c37f96a769f74bc792
Dohyeop Lim <dhlim0817@gmail.com>
2025-11-13 18:24:47 +0900

    fix: add nested routes

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,8 @@ createRoot(document.getElementById("root")!).render(
             <Routes>
                 <Route element={<Layout />}>
                     <Route index element={<Home />} />
-                    <Route path="party" element={<Party />}>
+                    <Route path="party">
+                        <Route index element={<Party />} />
                         <Route path="new" element={<NewParty />} />
                         <Route path="check-in" element={<CheckIn />} />
                     </Route>
